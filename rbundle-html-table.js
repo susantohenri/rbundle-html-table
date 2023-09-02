53255ec53c64513a9367b00b974ed5de9a334e87
Henri Susanto <henry.dinus@gmail.com>
2023-09-02 21:19:01 +0700

    custom row-count

--- rbundle-html-table.js
+++ rbundle-html-table.js
@@ -29,7 +29,10 @@ function rbundle_html_table_draw_header(table, dt) {
 }
 
 function rbundle_html_table_draw_body(table, dt) {
-    const row_count = table.attr(`row-count`)
+    var row_count = table.attr(`row-count`)
+    row_count = rbundle_html_table_special_case_custom_row_count(row_count, function () {
+        rbundle_html_table_draw_body(table, dt)
+    })
     const th_count = table.attr(`thead`).split(`,`).length
     const tbody = undefined !== table.attr(`tbody`) ? table.attr(`tbody`).split(`,`) : []
     var body = []
@@ -43,14 +46,15 @@ function rbundle_html_table_draw_body(table, dt) {
         }
     }
 
+    if (1 > body.length) return false
     dt.clear()
     dt.rows.add(body).draw()
     table.find(`tbody td`).attr(`contenteditable`, true)
-    rbundle_html_table_trash(table, dt)
-    rbundle_html_table_special_case_current_year_dash_index(tbody, table, dt)
+    rbundle_html_table_special_case_trash(table, dt)
+    rbundle_html_table_special_case_custom_tbody(tbody, table, dt)
 }
 
-function rbundle_html_table_translate_formula(formula, field_change_callback) {
+function rbundle_html_table_translate_formula(formula, redraw) {
     var result = ``
 
     // if not set
@@ -67,27 +71,27 @@ function rbundle_html_table_translate_formula(formula, field_change_callback) {
         const field = jQuery(`[name="item_meta[${formula.replace(`field`, ``)}]"]`)
         if (field.length > 0) {
             result = field.val()
-            field.change(field_change_callback)
+            field.change(redraw)
         }
     }
 
-    // show trash icon for deleting current row, keyword: 'delete-button'
-    else if (`delete-button` === formula) result = `<i class="fa-solid fa-trash"></i>`
+    // show trash icon for deleting current row, keyword: 'trash'
+    else if (`trash` === formula) result = `<i class="fa-solid fa-trash"></i>`
 
     return result
 }
 
-function rbundle_html_table_special_case_current_year_dash_index(tbody, table, dt) {
+function rbundle_html_table_special_case_custom_tbody(tbody, table, dt) {
     const td_index = tbody.indexOf(`current-year-dash-index`)
-    if (0 > td_index) return false
-
-    for (var tr_index = 0; tr_index < table.find(`tbody tr`).length; tr_index++) {
-        dt.cell({ row: tr_index, column: td_index }).data(`Current year - ${tr_index}`)
-        table.find(`tr`).eq(tr_index + 1).find(`td`).eq(td_index).removeAttr(`contenteditable`)
+    if (-1 < td_index) {
+        for (var tr_index = 0; tr_index < table.find(`tbody tr`).length; tr_index++) {
+            dt.cell({ row: tr_index, column: td_index }).data(`Current year - ${tr_index}`)
+            table.find(`tr`).eq(tr_index + 1).find(`td`).eq(td_index).removeAttr(`contenteditable`)
+        }
     }
 }
 
-function rbundle_html_table_trash(table, dt) {
+function rbundle_html_table_special_case_trash(table, dt) {
     table.find(`tbody tr td i.fa-solid.fa-trash`).each(function () {
         const trash = jQuery(this)
         const td = trash.parent()
@@ -97,4 +101,17 @@ function rbundle_html_table_trash(table, dt) {
             dt.row(tr).remove().draw(false)
         })
     })
+}
+
+function rbundle_html_table_special_case_custom_row_count(row_count, redraw_body) {
+
+    // row-count="current-year-minus-field840" => 2023 (current year) - 2022 (input value) + 1 (include current year) = 2 row
+    if (row_count.startsWith(`current-year-minus-`)) {
+        const field_id = row_count.replace(`current-year-minus-`, ``).replace(`field`, ``)
+        const field = jQuery(`[name="item_meta[${field_id}]"]`)
+        if (field.length > 0) {
+            field.change(redraw_body)
+            return parseInt((new Date()).getFullYear()) - parseInt(field.val()) + 1
+        } else return 0
+    } else return row_count
 }
